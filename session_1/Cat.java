03634d17aaa659adb480701784193c4c4833f5f6
Alina <78976903+alina714@users.noreply.github.com>
2021-03-07 14:25:07 +0100

    Update Cat.java

--- session_1/Cat.java
+++ session_1/Cat.java
@@ -3,4 +3,20 @@ import java.io.*;
 import java.util.Scanner;
 import javax.swing.ImageIcon;
 
-// Task 1: Create a Cat class
+// Task 1: Create a Cat cla
+public Cat(String name){
+    this.name = name;
+    this.setDescription();
+  }
+
+public String getDescription(){
+    return this.description;
+}
+
+public void setDescription(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter cat description: ");
+    String description = scanner.nextLine();
+    System.out.println(description);
+    this.description = description;
+}
